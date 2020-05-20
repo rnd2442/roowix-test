2d62bed49e959aa36838364d3c4acfa191389da6
Nikita Fedurin <nfedurinjobs@gmail.com>
2020-05-20 21:24:12 +0700

    Small refactor

--- src/components/PrimaryMap.tsx
+++ src/components/PrimaryMap.tsx
@@ -1,46 +1,37 @@
 import React, { useEffect, useRef } from "react";
-import { Map, ImageOverlay } from "react-leaflet";
-import { CRS } from "leaflet";
+import { Map } from "react-leaflet";
+import L, { CRS, LatLngTuple } from "leaflet";
 
-type TState = {
-  lat: number;
-  lng: number;
-  zoom: number;
-};
-
-const bounds: [number, number][] = [
+const bounds: LatLngTuple[] = [
   [1165, 0],
   [0, 1648],
 ];
 
 export const PrimaryMap: React.FC = () => {
-  const mapRef = useRef(null);
+  const mapRef = useRef<Map>(null);
+
   useEffect(() => {
-    // @ts-ignore
-    const map = mapRef.current.leafletElement;
-    // @ts-ignore
-    const image = L.imageOverlay(
-      "https://static.tildacdn.com/tild3965-6139-4564-b262-303737393665/_8.jpg",
-      bounds
-    ).addTo(map);
-    map.fitBounds(image.getBounds());
+    // Set bounds after map has mounted
+    if (mapRef && mapRef.current) {
+      // @ts-ignore
+      const map = mapRef.current.leafletElement;
+      const image = L.imageOverlay(
+        "https://static.tildacdn.com/tild3965-6139-4564-b262-303737393665/_8.jpg",
+        bounds
+      ).addTo(map);
+      map.fitBounds(image.getBounds());
+    }
   }, []);
 
   return (
     <Map
-      // @ts-ignore
       ref={mapRef}
-      minZoom={0}
-      maxZoom={2}
+      minZoom={1}
+      maxZoom={1}
       crs={CRS.Simple}
       maxBounds={bounds}
     >
-      {/* <ImageOverlay
-        bounds={bounds}
-        url={
-          "https://static.tildacdn.com/tild3965-6139-4564-b262-303737393665/_8.jpg"
-        }
-      /> */}
+      {}
     </Map>
   );
 };
