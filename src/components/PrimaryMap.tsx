4b8a82d7d3cfaf18f796b323cb7cc05c6850d659
Nikita Fedurin <nfedurinjobs@gmail.com>
2020-05-20 19:06:09 +0700

    map works again

--- src/components/PrimaryMap.tsx
+++ src/components/PrimaryMap.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Map, ImageOverlay, Marker, Popup, LayersControl } from "react-leaflet";
+import {
+  Map,
+  ImageOverlay,
+  ZoomControl,
+  Marker,
+  Popup,
+  LayersControl,
+  TileLayer,
+} from "react-leaflet";
 
 type TState = {
   lat: number;
@@ -9,9 +17,9 @@ type TState = {
 
 export const PrimaryMap: React.FC = () => {
   const [state, setState] = useState<TState>({
-    lat: 0,
-    lng: 0,
-    zoom: 1,
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
   });
 
   const a = 1648;
@@ -20,27 +28,45 @@ export const PrimaryMap: React.FC = () => {
   const position: [number, number] = [state.lat, state.lng];
 
   return (
-    <div id="mapid">
-      <Map
-        bounds={[
-          [0, 0],
-          [b, a],
-        ]}
-        // center={position}
-        // zoom={state.zoom}
-        center={[a / 2, b / 2]}
-        zoom={0}
-        maxZoom={10}
-        // style={{ width: "100vw", height: "100vw" }}
-      >
-        <ImageOverlay
-          url="https://static.tildacdn.com/tild3965-6139-4564-b262-303737393665/_8.jpg"
-          bounds={[
-            [0, 0],
-            [a, b],
-          ]}
-        />
-      </Map>
-    </div>
+    <Map center={position} zoom={state.zoom}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
   );
+
+  //   return (
+  //     <Map
+  //       // bounds={[
+  //       //   [0, 0],
+  //       //   [b, a],
+  //       // ]}
+  //       center={position}
+  //       // zoom={state.zoom}
+  //       //   center={[a / 2, b / 2]}
+  //       zoom={1}
+  //       maxZoom={10}
+  //       // style={{ width: "100vw", height: "100vw" }}
+
+  //       //   style={{ position: "static", top: 0, right: 0, bottom: 0, left: 0 }}
+  //     >
+  //       <ImageOverlay
+  //         url="https://static.tildacdn.com/tild3965-6139-4564-b262-303737393665/_8.jpg"
+  //         // bounds={[
+  //         //   [0, 0],
+  //         //   [a, b],
+  //         // ]}
+  //         bounds={[
+  //           [32, -130],
+  //           [13, -100],
+  //         ]}
+  //       />
+  //     </Map>
+  //   );
 };
